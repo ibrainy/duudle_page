a085395d42748127cacb3fd8d24682f80ddfff67
brainy <brainy@brainydeMacBook-Pro.local>
2016-10-26 17:08:11 +0800

    change port

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ pm2.connect(function() {
     instances : instances,
     max_memory_restart : maxMemory + 'M',   // Auto restart if process taking more than XXmo
     env: {                            // If needed declare some environment variables
-      "NODE_ENV": "production",
-      "PORT": 8080
+      "NODE_ENV": "production"
     },
     post_update: ["npm install"]       // Commands to execute once we do a pull from Keymetrics
   }, function() {
